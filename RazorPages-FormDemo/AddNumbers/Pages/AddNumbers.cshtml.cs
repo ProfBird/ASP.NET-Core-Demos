a5148574a99a720bdb044caf43f173808ba52edf
Brian Bird <birdb@lanecc.edu>
2017-10-02 10:03:41 -0700

    Initial commit
    This demo works!

---
+++ RazorPages-FormDemo/AddNumbers/Pages/AddNumbers.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AddNumbers.Pages
+{
+    public class AddNumbersModel : PageModel
+    {
+        public string TotalMessage { get; set; }
+        public IActionResult OnPost()
+        {
+            var total = 0.0;
+
+            // Retrieve the numbers that the user entered.
+            var num1 = Request.Form["text1"];
+            var num2 = Request.Form["text2"];
+
+            // Convert the entered strings into integers numbers and add.
+            total = double.Parse(num1) + double.Parse(num2);
+            TotalMessage = "Total = " + total;
+            return Page();
+        }
+    }
+}
