597d73cc95ae70325c2da678b75143a8edd4f7b5
Brian Bird <birdb@lanecc.edu>
2017-10-02 10:52:53 -0700

    Bug fixes
    I had to do a few things to fix the code. Now it all works!

--- WebPages3Tutorial/WebPages3Tutorial/Pages/AddNumbers.cshtml.cs
+++ WebPages3Tutorial/WebPages3Tutorial/Pages/AddNumbers.cshtml.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +5,19 @@ namespace WebPages3Tutorial.Pages
 {
     public class AddNumbersModel : PageModel
     {
-        public void OnGet()
+        public string TotalMessage { get; set; }
+        public IActionResult OnPost()
         {
+            var total = 0.0;
+
+            // Retrieve the numbers that the user entered.
+            var num1 = Request.Form["text1"];
+            var num2 = Request.Form["text2"];
+
+            // Convert the entered strings into integers numbers and add.
+            total = double.Parse(num1) + double.Parse(num2);
+            TotalMessage = "Total = " + total;
+            return Page();
         }
     }
 }
