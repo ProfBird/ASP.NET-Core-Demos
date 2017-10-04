738daec07df73153de388b500f75f1f31d1d6f33
Brian Bird <profbird@gmail.com>
2017-10-04 09:35:02 -0700

    Added a page
    Still not done, but it doesn’t crash

---
+++ WebPages3Tutorial/WebPages3Tutorial/Pages/CodeWithObjects.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebPages3Tutorial.Pages
+{
+    public class CodeWithObjectsModel : PageModel
+    {
+        public string RequestUrl { get; set; }
+		public string FilePath { get; set; }
+		public string MapPath { get; set; }
+		public string RequestType { get; set; }
+
+        public void OnGet()
+        {
+            RequestUrl = Url.Action();  // abosolute pathname for this action method
+            FilePath = Request.Path;
+            MapPath = Request.PathBase;
+            RequestType = Request.Protocol;
+
+        }
+    }
+}
