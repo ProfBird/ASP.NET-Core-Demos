51a28183cb29f00370e24d18906fb7592baade3e
Brian Bird <profbird@gmail.com>
2017-10-06 15:43:44 -0700

    Added a constructor test

--- Tic-Tac-Toe/TicTacToe.Test/UnitTest1.cs
+++ Tic-Tac-Toe/TicTacToe.Test/UnitTest1.cs
@@ -10,7 +10,19 @@ namespace TicTacToeGame.Test
         public void ConstructorTest()
         {
             var game = new TicTacToe();
-
+            Assert.Equal(0, game.TurnCount);
+            // Check to make sure all 9 squares are empty
+            bool allEmpty = true;
+            for (int r = 0; r < TicTacToe.ROWS; r++)
+                for (int c = 0; c < TicTacToe.COLS; c++)
+                    if (game.GetMark(r, c) !== ' ')
+                        allEmpty = false;
+            Assert.True(allEmpty);
         }
     }
 }
+
+/*
+    Facts are tests which are always true. They test invariant conditions.
+    Theories are tests which are only true for a particular set of data.
+*/
