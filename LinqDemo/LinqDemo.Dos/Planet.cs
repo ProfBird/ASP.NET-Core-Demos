f3380fc2d9987a3af89aeff3013f763bb980b313
Brian Bird <profbird@gmail.com>
2017-11-08 22:10:33 -0800

    Created a LINQ demo project

---
+++ LinqDemo/LinqDemo.Dos/Planet.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinqDemo.Dos
+{
+    public class Planet
+    {
+        public string Name {get; set;}
+        public int Diameter {get; set;}         // km
+        public int DistanceToSun {get; set;}    // millinon km
+    }
+}
