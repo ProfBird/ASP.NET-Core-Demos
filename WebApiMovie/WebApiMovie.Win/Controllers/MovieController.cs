5d061c78fbda56a433074370fc106077d60e37eb
Brian <birdb@lanecc.edu>
2017-11-22 15:54:59 -0800

    Finished the controller methods

--- WebApiMovie/WebApiMovie.Win/Controllers/MovieController.cs
+++ WebApiMovie/WebApiMovie.Win/Controllers/MovieController.cs
@@ -29,27 +29,71 @@ namespace WebApiMovie.Win.Controllers
 
         // GET api/movie/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var movies = from m in _context.Movie
+                         where id == m.ID
+                         select m;
+
+           return new ObjectResult(
+                movies.SingleOrDefault<Movie>());
         }
 
         // POST api/movie
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]Movie movie)
         {
+            if (movie == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Movie.Add(movie);
+            _context.SaveChanges();
+
+            return new ObjectResult(movie);
+            //return CreatedAtRoute("Get", new { id = movie.ID }, movie);
         }
 
         // PUT api/movie/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody] Movie movie)
         {
+            if (movie == null || movie.ID != id)
+            {
+                return BadRequest();
+            }
+
+            var movieToChange = _context.Movie.FirstOrDefault(m => m.ID == id);
+            if (movieToChange == null)
+            {
+                return NotFound();
+            }
+
+            movieToChange.Genre = movie.Genre;
+            movieToChange.Price = movie.Price;
+            movieToChange.Rating = movie.Rating;
+            movieToChange.ReleaseDate = movie.ReleaseDate;
+            movieToChange.Title = movie.Title;
+
+            _context.Movie.Update(movieToChange);
+            _context.SaveChanges();
+            return new ObjectResult(movieToChange);
         }
 
         // DELETE api/movie/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var movie = _context.Movie.FirstOrDefault(m => m.ID == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movie.Remove(movie);
+            _context.SaveChanges();
+            return new NoContentResult();
         }
     }
 }
